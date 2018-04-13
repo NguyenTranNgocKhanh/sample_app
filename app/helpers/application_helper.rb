1cd0022ab9e123d41cf6651a5a23fe5a9fd61361
Nguyentranngockhanh <ngtrngockhanh2197@gmail.com>
2018-04-13 16:36:44 +0700

    Finish chapter 6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
-  def full_title page_title = ""
-    base_title = I18n.t ".title"
-    if page_title.empty?
+  def full_title page_title
+    base_title = I18n.t "helper.application_helper.title"
+    if page_title.blank?
       base_title
     else
       page_title + " | " + base_title
